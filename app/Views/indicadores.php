546d5b81afaa9e70328c77f8be7a0df0d2aececb
Matias <maticou123@gmail.com>
2023-03-14 15:57:37 -0300

    changes on css

--- app/Views/indicadores.php
+++ app/Views/indicadores.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/styles.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap/customize-css/styles.css'); ?>">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
